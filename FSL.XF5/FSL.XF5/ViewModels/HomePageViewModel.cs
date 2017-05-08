f6ccda7c90c7ca535ca6b38b5a21a34b1f574e5f
Fabio Silva Lima <fabiosilvalima@terra.com.br>
2017-05-08 08:11:51 -0300

    FIRST COMMIT

---
+++ FSL.XF5/FSL.XF5/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSL.XF5.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class HomePageViewModel
+    {
+        public string Titulo { get; set; }
+    }
+}
---
+++ FSL.XF5/FSL.XF5/Views/HomePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace FSL.XF5.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        ViewModels.HomePageViewModel vm = null;
+
+        public HomePage()
+        {
+            vm = new ViewModels.HomePageViewModel()
+            {
+                Titulo = "Ola Mundo"
+            };
+
+            BindingContext = vm;
+
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            vm.Titulo += " via botão";
+        }
+    }
+}
